0cfa67c94a34b7fe9058247a014ededbabe96496
sami <samiap914@gmail.com>
2024-05-06 05:23:26 +0530

    basic home page

---
+++ academy/src/app/Home/Home.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import './Home.css'
+
+import { Link } from 'react-router-dom'
+import careHome from '../../assets/care.png'
+
+const Home = () => {
+
+  return (
+ <div className="home" style ={{ backgroundImage: `url(${careHome})` }}>
+        <div className="headerContainer" >
+            <h1>care connect</h1>
+            <p>care made simple</p>
+            <Link to="/signup">
+               <button>SIGN UP</button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Home
--- academy/src/app/page.js
+++ academy/src/app/page.js
@@ -1,8 +1,35 @@
-export default function Home() {
+
+
+import Link from 'next/link'
+import Image from 'next/image'
+import academyhome from '../../public/academyhome.jpg'
+
+
+export default function Home (){
+
   return (
-   <div className="text-center h-1/2">
-    <h1>Welcome to Academy</h1>
-    <button>Sign in</button> <button>Sign up</button>
-   </div>
-  );
+    <div className='flex h-screen'>
+      <div>
+    <Image
+     src="/academyhome.jpg"
+    alt="Picture"
+    width={900}
+    height={900}
+    priority
+    />
+    </div>
+    <div className="text-center w-1/2 items-center justify-center h-1/2" >
+    <h1 className='text-50 mt-80 text-blue-400'>ACADEMY</h1>
+    
+    <Link href="/signup">
+       <button class="my-2 rounded-lg bg-blue-300 px-4 py-2 text-white hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-50 active:bg-blue-700">
+    SIGN UP
+  </button>
+    </Link>
+</div>
+
+       
+    </div>
+  )
 }
+
